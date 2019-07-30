bba23bcc8efaf14818f559e8980cbeced201f3c7
jasonymy <jasonymy@yahoo.com.tw>
2019-07-30 14:06:39 +0800

    Update ValuesController.cs

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace GitHub.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "OpenShift", "GBT SIT Demo" };
         }
 
         // GET api/values/5
